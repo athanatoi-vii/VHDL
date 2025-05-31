7b174620ef74197e7124b94305ed1c30a00d38b2
iman latifi (40021441054120) <109227583+athanatoi-vii@users.noreply.github.com>
2025-05-31 15:10:09 +0330

    Update T_8.vhdl

--- T_8.vhdl
+++ T_8.vhdl
@@ -3,25 +3,25 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity Tlightemoore is
-    generic (
+entity TrafficLight is
+    generic
+    (
         TIME_START : INTEGER := 0;
         TIME_END   : INTEGER := 120
     );
-    port(
+    port
+    (
         clk, reset : in STD_LOGIC;
         ta, tb     : in STD_LOGIC;
         la, lb     : out STD_LOGIC_VECTOR(1 DOWNTO 0)
     );
 end entity;
 
-architecture Behavioral of Tlightemoore is
-    type statetype is (s0, s1, s2, s3);
+architecture Behavioral of TrafficLight is
+    type statetype is (s0, s1, s2);
     signal state, nextstate : statetype;
     signal counter : INTEGER range 0 to 200 := TIME_START;
 begin
-
-    
     process(clk, reset)
     begin
         if reset = '1' then
@@ -31,7 +31,6 @@ begin
         end if;
     end process;
 
-    
     process(clk, reset)
     begin
         if reset = '1' then
@@ -47,7 +46,6 @@ begin
         end if;
     end process;
 
-    
     process(state, ta, tb, counter)
     begin
         case state is
@@ -59,37 +57,28 @@ begin
                 else
                     nextstate <= s0;
                 end if;
-
             when s1 =>
                 nextstate <= s2;
-
             when s2 =>
                 if tb = '1' then
                     nextstate <= s2;
                 elsif tb = '0' or counter = TIME_END then
-                    nextstate <= s3;
+                    nextstate <= s0;
                 else
                     nextstate <= s2;
                 end if;
-
-            when s3 =>
-                nextstate <= s0;
-
             when others =>
                 nextstate <= s0;
         end case;
     end process;
-
-    
+ 
     la <= "00" when state = s0 else 
           "01" when state = s1 else  
           "10" when state = s2 else  
           "10";                     
-
    
     lb <= "10" when state = s0 else  
           "10" when state = s1 else  
           "00" when state = s2 else  
-          "01";                     
-
+          "01";                 
 end Behavioral;
